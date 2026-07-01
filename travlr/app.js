56fef9505166642fc9e4e794f245813e1a5b4efd
Kelly Reinersman <Kelly.Reinersman@snhu.edu>
2026-06-30 20:07:16 -0500

    Fix SonarQube warnings: node:path, disable x-powered-by, contact.html a11y

--- travlr/app.js
+++ travlr/app.js
@@ -1,8 +1,12 @@
-const path = require('path');
+const path = require('node:path');
 const express = require('express');
 
 const app = express();
 const PORT = 3000;
+
+// Hide the "X-Powered-By: Express" response header - SonarQube flags this
+// because it discloses server framework/version info to anyone probing the site
+app.disable('x-powered-by');
 
 // Serve everything in this folder as static files
 app.use(express.static(path.join(__dirname)));
