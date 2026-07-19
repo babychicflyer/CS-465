dc68ea8231b102e2a636f529e2c00f26a8291811
Kelly Reinersman <Kelly.Reinersman@snhu.edu>
2026-07-19 16:43:18 -0500

    Module 3 Completed Baseline

--- travlr/app.js
+++ travlr/app.js
@@ -1,16 +1,47 @@
-const path = require('node:path');
+const createError = require('http-errors');
 const express = require('express');
+const path = require('node:path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const hbs = require('hbs');
+
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
 
 const app = express();
-const PORT = 3000;
-
-// Hide the "X-Powered-By: Express" response header - SonarQube flags this
-// because it discloses server framework/version info to anyone probing the site
 app.disable('x-powered-by');
 
-// Serve everything in this folder as static files
-app.use(express.static(path.join(__dirname)));
+// view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-app.listen(PORT, () => {
-  console.log(`Travlr Getaways server running at http://localhost:${PORT}`);
+// Set up handlebars view engine
+hbs.registerPartials(__dirname + '/app_server/views/partials');
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, () => {
+  console.log('Travlr Getaways server running at http://localhost:3000');
 });
